9ce1c42b4b46ab3a7d754bc4cb7e762311b3d583
jamerson walderson <jamersonwalderson@gmail.com>
2022-05-08 20:00:11 -0300

    feat: criação da página signIn

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { style } from "./style";
+
+export function SignIn() {
+  return (
+    <View style={style.container}>
+      <Text>Bem vindo ao APP</Text>
+    </View>
+  );
+}
