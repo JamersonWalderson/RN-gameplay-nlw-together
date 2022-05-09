84131c9de3df169ab1eeaf894a0072adeea09812
jamerson walderson <jamersonwalderson@gmail.com>
2022-05-08 21:03:34 -0300

    feat: adicionando assets

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,11 +4,17 @@ import { style } from "./style";
 
 export function SignIn() {
   const [text, setText] = useState('');
+  
   return (
     <View style={style.container}>
       <Text>Bem vindo ao APP</Text>
 
-      <TextInput style={style.input} onChangeText={setText} value={text} />
+      <TextInput
+        style={style.input}
+        onChangeText={setText}
+        value={text}
+      />
+
       <Text>
         Voce digitou: ... {text}
       </Text>
